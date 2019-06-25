6405d30e4b862ba83aecf1819858b9d728c2b352
Moritz Warning <moritzwarning@web.de>
2019-06-25 17:58:39 +0200

    fix qr code scanner

--- app/src/main/java/com/example/trigger/QRScanActivity.java
+++ app/src/main/java/com/example/trigger/QRScanActivity.java
@@ -8,13 +8,18 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.zxing.BarcodeFormat;
 import com.google.zxing.ResultPoint;
 import com.journeyapps.barcodescanner.BarcodeCallback;
 import com.journeyapps.barcodescanner.BarcodeResult;
 import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+import com.journeyapps.barcodescanner.DefaultDecoderFactory;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import org.json.JSONException;
@@ -27,20 +32,57 @@ public class QRScanActivity extends AppCompatActivity implements BarcodeCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_qrscan);
+        barcodeView = (DecoratedBarcodeView) findViewById(R.id.barcodeScannerView);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+        } else {
+            startScan();
         }
+    }
+
+    private void startScan() {
+        Collection<BarcodeFormat> formats = Collections.singletonList(BarcodeFormat.QR_CODE);
+        barcodeView.getBarcodeView().setDecoderFactory(new DefaultDecoderFactory(formats));
+        barcodeView.initializeFromIntent(getIntent());
+        barcodeView.decodeContinuous(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        barcodeView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        barcodeView.pause();
+    }
+
+    public void pause(View view) {
+        barcodeView.pause();
+    }
+
+    public void resume(View view) {
+        barcodeView.resume();
+    }
+
+    public void triggerScan(View view) {
+        barcodeView.decodeSingle(this);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
-            //bindService(new Intent(this, MainService.class), this, Service.BIND_AUTO_CREATE);
+            startScan();
         } else {
-            Toast.makeText(this, "Camera permission Request", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Camera permissons required for QR code scan.", Toast.LENGTH_LONG).show();
             finish();
         }
     }
@@ -48,7 +90,6 @@ public class QRScanActivity extends AppCompatActivity implements BarcodeCallback
     @Override
     public void barcodeResult(BarcodeResult result) {
         String json = result.getText();
-        Log.d("QRScanActivity", json);
 
         try {
             JSONObject obj = new JSONObject(json);
