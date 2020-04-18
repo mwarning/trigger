01630ff5de54e30c7df7feb7d8afd8c0bae4a3e0
Moritz Warning <moritzwarning@web.de>
2020-04-18 20:41:19 +0200

    remove ACCESS_FINE_LOCATION
    was only used during bluetooth testing

--- app/src/main/java/com/example/trigger/MainActivity.java
+++ app/src/main/java/com/example/trigger/MainActivity.java
@@ -414,11 +414,6 @@ public class MainActivity extends AppCompatActivity implements OnTaskCompleted {
             cloneMenuItem.getIcon().setAlpha(130);
         }
 
-        if (ContextCompat.checkSelfPermission(
-                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 123);
-        }
-
         return true;
     }
 
