d4f54122acf1993fc03401b486d567b4c914a0fc
Moritz Warning <moritzwarning@web.de>
2022-03-04 12:08:51 +0100

    bump version to 3.4.2

--- app/src/main/java/app/trigger/Settings.java
+++ app/src/main/java/app/trigger/Settings.java
@@ -311,6 +311,15 @@ public class Settings {
             sharedPreferences.edit().putString("db_version", new_version).commit();
             db_version = new_version;
         }
+
+        if (Arrays.asList("3.4.1").contains(db_version)) {
+            String new_version = "3.4.2";
+            Log.i(TAG, "Update database format from " + db_version + " to " + new_version);
+            // nothing to change
+            setups = new ArrayList();
+            sharedPreferences.edit().putString("db_version", new_version).commit();
+            db_version = new_version;
+        }
     }
 
     static void init(Context context) {
